5772e64c880c3073a55b8462c24617ccda32597d
Andrew Menning <menning23@gmail.com>
2016-06-01 20:41:29 -0400

    declaration example

---
+++ 3_Declaration.sql
@@ -0,0 +1,10 @@
+set serveroutput on
+DECLARE
+	-- variable declaration 
+	num1 INTEGER; -- ANSI and IBM specific integer type with maximum precision of 38 decimal digits
+	num2 REAL; -- Floating-point type with maximum precision of 63 binary digits (approximately 18 decimal digits)
+	num3 DOUBLE PRECISION; -- ANSI specific floating-point type with maximum precision of 126 binary digits (approximately 38 decimal digits)ANSI specific floating-point type with maximum precision of 126 binary digits (approximately 38 decimal digits)
+BEGIN
+	null;
+END;
+/
