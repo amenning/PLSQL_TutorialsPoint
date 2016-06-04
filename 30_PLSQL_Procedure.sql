18bddbb678796b0b9e03bed5b3d75be81e4fbee0
Andrew Menning <menning23@gmail.com>
2016-06-04 10:03:13 -0400

    PLSQL procedure example

---
+++ 30_PLSQL_Procedure.sql
@@ -0,0 +1,20 @@
+/* PL/SQL Procedure
+
+PL/SQL subprograms are named PL/SQL blocks that can be invoked with a set of parameters. PL/SQL provides two kinds of subprograms:
+
+Functions: these subprograms return a single value, mainly used to compute and return a value.
+
+Procedures: these subprograms do not return a value directly, mainly used to perform an action.
+*/
+set serveroutput on
+CREATE OR REPLACE PROCEDURE greetings
+AS
+BEGIN
+	dbms_output.put_line('Hello World!');
+END;
+/
+-- call new procedure greetings or use "EXECUTE greetings" at command line interface
+BEGIN
+	greetings;
+END;
+/
